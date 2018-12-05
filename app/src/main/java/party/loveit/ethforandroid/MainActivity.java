48784c83a613493dc62ba72f7f096d0ad084f49b
wangyunpeng@snail.com <wangyunpeng@snail.com>
2018-12-05 16:46:19 +0800

    修改上传jcenter配置

--- app/src/main/java/party/loveit/ethforandroid/MainActivity.java
+++ app/src/main/java/party/loveit/ethforandroid/MainActivity.java
@@ -2,6 +2,15 @@ package party.loveit.ethforandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import party.loveit.bip44forandroidlibrary.utils.Bip44Utils;
+import party.loveit.ethforandroidlibrary.crypto.ECKeyPair;
+import party.loveit.ethforandroidlibrary.crypto.Keys;
+import party.loveit.ethforandroidlibrary.utils.Numeric;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    List<String> words = Bip44Utils.generateMnemonicWords(MainActivity.this);
+                    Log.e("TAG", "words: " + words.toString());
+
+                    //m / purpose’ / coin_type’ / account’ / change / address_index
+                    BigInteger priKey = Bip44Utils.getPathPrivateKey(words, "m/44'/60'/0'/0/0");
+
+                    ECKeyPair ecKeyPair = ECKeyPair.create(priKey);
+                    String publicKey = Numeric.toHexStringWithPrefix(ecKeyPair.getPublicKey());
+                    Log.e("TAG", "publicKey: " + publicKey);
+                    String privateKey = Numeric.toHexStringWithPrefix(ecKeyPair.getPrivateKey());
+                    Log.e("TAG", "privateKey: " + privateKey);
+                    String address = "0x" + Keys.getAddress(ecKeyPair);
+                    Log.e("TAG", "address: " + address);
+
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
